9decdcd99cfa1719fdcb9bd90290787c003a82a6
galkin <galkin@home>
2016-03-26 04:27:45 +0200

    hhh

--- mavenproject3/src/main/java/ua/unicyb/mavenproject3/MyServlet.java
+++ mavenproject3/src/main/java/ua/unicyb/mavenproject3/MyServlet.java
@@ -38,7 +38,7 @@ public class MyServlet extends HttpServlet {
             out.println("<title>Servlet MyServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet MyServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet MyServlet at lll " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
